35645803db9eeb510a0d9824e4057fe68348ec98
Zeroni-IT <traidenet@i.ua>
2016-04-12 00:43:22 +0300

    home work JS #9-10 added

---
+++ JS_Home_Work_09-10/script/script.js
@@ -0,0 +1,78 @@
+$(function () {
+
+    var showDropdown = function () {
+        $(this).children('.sub_menu')
+            .show(200)
+            .animate({
+                backgroundColor: "green",
+            }, 200)
+        ;
+    };
+
+    var hideDropdown = function () {
+        $(this).children('.sub_menu')
+            .hide(200)
+            .animate({
+                backgroundColor: "lime",
+            }, 200)
+        ;
+    };
+
+    $('li.main_dropdown').hover(showDropdown, hideDropdown);
+
+    var changeColor = function () {
+        $(this).animate({
+            color: 'white',
+            fontWeight: 900,
+        }, 100);
+    };
+
+    var changeColorBack = function () {
+        $(this).animate({
+            color: 'white',
+            fontWeight: 100,
+        }, 100);
+    };
+
+    $('.sub_menu--link').hover(changeColor, changeColorBack);
+
+    $('.jcarousel')
+        .jcarousel({
+            animation: 'slow',
+            wrap: 'circular'
+        })
+
+        .jcarouselAutoscroll({
+            interval: 3000,
+            target: '+=1',
+            autostart: true
+        })
+    ;
+
+    $('.jcarousel_prev').jcarouselControl({
+        target: '-=1'
+    });
+
+    $('.jcarousel_next').jcarouselControl({
+        target: '+=1'
+    });
+
+    $('.jcarousel_pagination')
+        .jcarouselPagination({
+            item: function (page) {
+                return '<a class = "jcarousel_page" href="#' + page + '"></a>';
+            }
+        })
+
+        .on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('active');
+        })
+
+        .on('jcarouselpagination:inactive', 'a', function () {
+            $(this).removeClass('active');
+        })
+    ;
+
+    $('.jquery_checkbox input, select').styler();
+
+});
